2dfdb45c16d2d79315d316febb36021b50f2cea5
Jene Hojin Choi <jenehojinchoi@gmail.com>
2021-03-19 14:33:20 +0900

    MODIFY: leetcode/100.py

--- leetcode/100.py
+++ leetcode/100.py
@@ -5,12 +5,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+## Approach 1. Iteration 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if (p and not q) or (q and not p): 
-            return False
-        if (p and q) and (p.val != q.val):
-            return False
         if not p and not q:
             return True
+        if not p or not q: 
+            return False
+        if p.val != q.val:
+            return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right)
+
+## Approach 2. 
+# from collections import deque 
+
+# class Solution:
+#     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+#         def check(p, q):
+#             if not p and not q:
+#                 return True
+#             if not p or not q:
+#                 return False
+#             if p.val != q.val:
+#                 return False
+#             return True
+
+#         deq = deque([(p,q),])
+#         while deq:
+#             p, q = deq.popleft()
+#             if not check(p, q):
+#                 return False
+#             if p:
+#                 deq.append((p.left, q.left))
+#                 deq.append((p.right, q.right))
+#         return True
