860be569be113a1feeb0f7a2212b49e6b4a4240b
jenehojinchoi <jenehojinchoi@gmail.com>
2022-04-04 15:21:33 -0700

    modify: leetcode/200.py

--- leetcode/200.py
+++ leetcode/200.py
@@ -13,7 +13,7 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
-                    grid[i][j] = 0
+                    grid[i][j] = '#'
                     queue.append((i,j))
                     self.bfs(grid,queue)
                     count += 1
@@ -29,7 +29,7 @@ class Solution:
             for i,j in [I-1,J],[I+1,J],[I,J-1],[I,J+1]:
                 if 0<= i < m and 0 <= j < n and grid[i][j] == '1':
                     queue.append((i,j))
-                    grid[i][j] = 0
+                    grid[i][j] = '#'
 
 
 # Approach 2. DFS
