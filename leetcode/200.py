18c2b4a80d662d1dd86d42615475e14043b35904
jenehojinchoi <jenehojinchoi@gmail.com>
2022-04-04 15:20:51 -0700

    modify: leetcode/200.py

--- leetcode/200.py
+++ leetcode/200.py
@@ -1,5 +1,6 @@
 # 200. Number of Islands
 
+# Approach 1. BFS
 from collections import deque
 class Solution:
     def numIslands(self, grid):
@@ -29,3 +30,31 @@ class Solution:
                 if 0<= i < m and 0 <= j < n and grid[i][j] == '1':
                     queue.append((i,j))
                     grid[i][j] = 0
+
+
+# Approach 2. DFS
+class Solution:
+    def check(self, grid, i, j):
+        # 내가 0 혹은 visited 거나 주변이 다 wall일때
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        
+        # 내가 1일때
+        grid[i][j] = '#'
+        self.check(grid, i+1, j)
+        self.check(grid, i, j+1)
+        self.check(grid, i-1, j)
+        self.check(grid, i, j-1)
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        count = 0 
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.check(grid, i, j)
+                    count += 1
+        return count
